1afcef146d5a961be6b08779ea88dbb63c794152
player_zero <playerzero745@gmail.com>
2024-12-08 13:04:02 +0000

    design the dashboard to be in column with pie chart

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -15,51 +15,58 @@ export class Tab2Page {
     options = {};
 	width = 600;
 	height = 400;
+	spend: [string, { category: string, amount: number, date: Date }][] = [];
 
-constructor(private storage: Storage) {}
+	constructor(private storage: Storage) {}
 
 	async ngOnInit() {
-	// Connect to database	
-	this.storage = await this.storage.create();
-    this.useAngularLibrary();
-	 this.listSpend();
-  }
+		// Connect to database	
+		this.storage = await this.storage.create();
+		this.useAngularLibrary();
+		this.listSpend();
+	}
 
-  async useAngularLibrary() {
+  	async useAngularLibrary() {
 
-	// Retrieve all keys in storage
-    const keys = await this.storage.keys();
-	console.log(keys);
-	// get value by key
-	 for (const key of keys) {
-		var totalExpenditure = 0;
-		const expenditures = await this.storage.get(key);
-		
-		for(const e of expenditures){
-			const number= Number(e.substring(0, e.indexOf('*')));
-			totalExpenditure= totalExpenditure + number;
+		// Retrieve all keys in storage
+		const keys = await this.storage.keys();
+		console.log(keys);
+		// get value by key
+		for (const key of keys) {
+			var totalExpenditure = 0;
+			const expenditures = await this.storage.get(key);
+			
+			for(const e of expenditures){
+				const number= Number(e.substring(0, e.indexOf('*')));
+				totalExpenditure= totalExpenditure + number;
+			}
+			this.pieChartData.push([key, totalExpenditure])
 		}
-		this.pieChartData.push([key, totalExpenditure])
-	 }
 
-	 console.log("pie chart" + this.pieChartData);
-  }
+		console.log("pie chart" + this.pieChartData);
+	}
 
-  spend: [string, { category: string, amount: number }][] = [];
- async listSpend(){
-
-	const keys = await this.storage.keys();
-	console.log(keys);
-
-	for (const key of keys) {
-		const spendItem = await this.storage.get(key);
- 
- 	for(const list of spendItem){
-		const details= list.substring(0, list.indexOf('*'));
-		this.spend.push([key, details])
-}
-
-}
- }
+	async listSpend() {
+		const keys = await this.storage.keys(); // Retrieve all keys
+	  
+		for (const key of keys) {
+		  const spendItems = await this.storage.get(key); // Retrieve the array of spend items for the current key
+	  
+		  for (const list of spendItems) {
+			// Extract the amount and date from the item
+			const amountString = list.substring(0, list.indexOf('*'));
+			const dateString = list.substring(list.indexOf('*') + 1);
+	  
+			// Parse the amount as a number and the date as a Date object
+			const amount = Number(amountString);
+			const date = new Date(dateString);
+	  
+			// Push the data to the spend array
+			this.spend.push([key, { category: key, amount, date }]);
+			console.log([key, { category: key, amount, date }]);
+		  }
+		}
+	}
+	  
 }
 
